ab6ac2060038961d3630f8899da2c0dd1d1b71d7
Anastasia Krapivina <krapivina934@gmail.com>
2023-04-08 14:48:10 +0700

    Cheange two

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,3 +1,21 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StatisticsServiceTest {
 
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
+
